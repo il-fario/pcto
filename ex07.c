c26a8980e07f5fb048317788bb75eaa4723eec6b
il-fario <128369669+il-fario@users.noreply.github.com>
2023-03-20 16:11:25 +0100

    Add files via upload

---
+++ ex07.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+    
+    printf("inserisci un numero");
+    scanf("%d", &x);
+
+    printf("inserisci un numero");
+    scanf("%d", &y);
+ 
+ if(x%y==0)
+{
+    printf("è un multiplo");
+}
+
+}
