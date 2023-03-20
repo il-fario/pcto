6129543c86ebd0c88e753402ff8115d586f69a0b
il-fario <128369669+il-fario@users.noreply.github.com>
2023-03-20 12:36:02 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+{
+   float o = 28; //flag ; %f
+   char e = 'E'; //flag ; %c
+   char l = 'L'; //flag ; %c
+   int q = 11; //flag ; %c
+   printf("ho creato una variabile float con valore %f, il primo carattere %c è uguale al secondo carattere %c, ho creato una terza variabile int con valore %c/n", o, e, l, q);
+}
