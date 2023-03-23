6fef2d1f8730b40a36d73125e4472781764ea4bf
sancho-05 <128369777+sancho-05@users.noreply.github.com>
2023-03-23 09:49:16 +0100

    Update ex28.c

--- ex28.c
+++ ex28.c
@@ -1,32 +1,5 @@
-#include <stdio.h>
 
-int primo (int x)
-{
-    int y = x-1;
-    while(y>1)
-    {
-    if(x%y==0)
-    {
-      return (0);
-    }
-    y--;
-    }
-    if(y==1)
-    {
-        return (1);
-    }
- }
 
- int main()
- {
-  int w;
-    printf("inserisci un numero");
-    scanf("%d", &w);
-    int a [w];
-    int i=0;
-    int p = 2;
-    while(i<w)
-    {
         if(primo(p))
         {
             a[i]=p;
